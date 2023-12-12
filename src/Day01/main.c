8979e5231e911d101009c7f2361d0666d03d4c04
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-12-12 10:38:03 +0100

    Day 1 part 2

--- src/Day01/main.c
+++ src/Day01/main.c
@@ -48,6 +48,61 @@ int sum_array(char *array[])
     return sum;
 }
 
+int sum_spelled(char *array[])
+{
+    int sum = 0;
+    for (int i = 0; i < FILE_LENGTH; i++)
+    {
+        int first = 0, last;
+        for (int j = 0; j < strlen(array[i]); j++)
+        {
+            switch (array[i][j])
+            {
+            case 'o':
+                if (array[i][j + 1] == 'n' && array[i][j + 2] == 'e')
+                    array[i][j] = '1';
+                break;
+            case 't':
+                if (array[i][j + 1] == 'w' && array[i][j + 2] == 'o')
+                    array[i][j] = '2';
+                if (array[i][j + 1] == 'h' && array[i][j + 2] == 'r' && array[i][j + 3] == 'e' && array[i][j + 4] == 'e')
+                    array[i][j] = '3';
+                break;
+            case 'f':
+                if (array[i][j + 1] == 'o' && array[i][j + 2] == 'u' && array[i][j + 3] == 'r')
+                    array[i][j] = '4';
+                if (array[i][j + 1] == 'i' && array[i][j + 2] == 'v' && array[i][j + 3] == 'e')
+                    array[i][j] = '5';
+                break;
+            case 's':
+                if (array[i][j + 1] == 'i' && array[i][j + 2] == 'x')
+                    array[i][j] = '6';
+                if (array[i][j + 1] == 'e' && array[i][j + 2] == 'v' && array[i][j + 3] == 'e' && array[i][j + 4] == 'n')
+                    array[i][j] = '7';
+                break;
+            case 'e':
+                if (array[i][j + 1] == 'i' && array[i][j + 2] == 'g' && array[i][j + 3] == 'h' && array[i][j + 4] == 't')
+                    array[i][j] = '8';
+                break;
+            case 'n':
+                if (array[i][j + 1] == 'i' && array[i][j + 2] == 'n' && array[i][j + 3] == 'e')
+                    array[i][j] = '9';
+                break;
+            }
+            if (isdigit(array[i][j]) && first == 0)
+            {
+                first = array[i][j] - '0';
+            }
+            if (isdigit(array[i][j]))
+            {
+                last = array[i][j] - '0';
+            }
+        }
+        sum += (first * 10 + last);
+    }
+    return sum;
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc != 2)
@@ -69,6 +124,7 @@ int main(int argc, char const *argv[])
     // for (int i = 0; i < FILE_LENGTH; i++)
     // {
     printf("Sum of all values from input file: %d\n", sum_array(lines));
+    printf("Sum of all spelled values from input file: %d\n", sum_spelled(lines));
     // }
 
     _fcloseall();
